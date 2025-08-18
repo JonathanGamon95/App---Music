b6f8c295d84ebd4948d85cd0dcb11810cbf0aefc
Jonathan Gamon <jonygamon@gmail.com>
2025-08-18 17:50:54 -0300

    correcciones

--- src/componentes/sections/artist-section/artist-section.jsx
+++ src/componentes/sections/artist-section/artist-section.jsx
@@ -1,10 +1,11 @@
 // CREACION DE ARTISTAS
+import imageArtist from "../../../assets/imagen/4a.jpg"
 
 function ArtistSection({artist, count}) {
 
     return (
         <li className="aspect-square bg-[green] relative rounded-xl select-none overflow-hidden text-sm">
-            <img src="./src/assets/imagen/4a.jpg" alt="imaegn-album"></img>
+            <img src={imageArtist} alt="imaegn-album"></img>
             <div className="absolute bottom-2 left-2 flex flex-col gap-1">
                 <h1>{artist}</h1>
                 <div className="flex gap-2">
--- src/componentes/sections/gerder-section/gerder-section.jsx
+++ src/componentes/sections/gerder-section/gerder-section.jsx
@@ -1,11 +1,12 @@
 // CREACION DE GENEROS:
+import imagenGerder from "../../../assets/imagen/1a.jpg"
 
 function GenresSection({ gerder, count }) {
 
     return (
         <>
             <li className="aspect-square bg-[green] relative rounded-xl select-none overflow-hidden text-sm">
-                <img src="./src/assets/imagen/1a.jpg" alt="imaegn-album"></img>
+                <img src={imagenGerder} alt="imaegn-album"></img>
                 <div className="absolute bottom-2 left-2 flex flex-col gap-1">
                     <h1>{gerder}</h1>
                     <div className="flex gap-2">
